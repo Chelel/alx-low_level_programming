bd55186b17502f695ad1f2fee7b6c88631b90bb4
Chelel <korir.chelel@gmail.com>
2023-01-21 05:20:38 -0800

    a function that prints anything.

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,13 +6,21 @@
  * Return: sum of its parameters.
  */
 
-int sum_them_all(const unsigned int n, ...) {
-    int sum = 0;
-    va_list args;
-    va_start(args, n);
-    for (unsigned int i = 0; i < n; i++) {
-        sum += va_arg(args, int);
-    }
-    va_end(args);
-    return sum;
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(valist, int);
+
+	va_end(valist);
+
+	return (sum);
 }

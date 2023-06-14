accb3a4200725eb60d9f40f060445992655d44b1
Chelel <korir.chelel@gmail.com>
2023-06-14 11:50:10 -0700

    betty-doc validation

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -4,6 +4,8 @@
  * key_index - function that returns the index of a key.
  * @key: pointer to key to get index.
  * @size: size of the array of the hash table.
+ * Return: the index at which the key/value pair should be stored in the array
+ *  of the hash table
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
